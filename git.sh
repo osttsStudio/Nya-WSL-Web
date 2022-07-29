e914431b23f3283ce4975efd6a67ada0f7e971ce
Nya-WSL <Nya-WSL@nya-wsl.com>
2022-07-30 01:28:19 +0800

    update

--- git.sh
+++ git.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 cp -r /opt/hexo/source /opt/Nya-WSL-Web
 cp -r /opt/hexo/source /opt/Nya-WSL-Web-Private
 
-cd /opt/Nya-WSL-Web
+function git_command {
 git add .
 git commit -am "update"
 git push
+}
 
+cd /opt/Nya-WSL-Web
+git_command
 cd /opt/Nya-WSL-Web-Private
-git add .
-git commit -am "update"
-git push
+git_command
