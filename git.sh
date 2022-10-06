cfb0039b51b6d799cdbb68979a3992ec76f32e62
Nya-wsl-HK <hk@mail.nya-wsl.com>
2022-10-06 23:48:18 +0800

    update

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -r /opt/hexo/source /opt/Nya-WSL-Web
 cp -r /opt/hexo/source /opt/Nya-WSL-Web-Private
+cp -r /etc/nginx/sites-available/default /opt/Nya-WSL-Web-Private/nginx_conf
 
 function git_command {
 git add .
